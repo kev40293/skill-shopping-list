523c42a8596a19ebec917d31143a882a1f4c81ba
Kevin Brandstatter <kevin@kbrandstatter.com>
2018-05-05 13:05:30 -0500

    Basic regex and dialog for add item to list

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
 
 
 class ShoppingListSkill(MycroftSkill):
@@ -10,9 +11,21 @@ class ShoppingListSkill(MycroftSkill):
     def handle_shopping_list(self, message):
         self.speak_dialog('shopping.list')
 
-    @intent_handler(IntentBuilder("").require('AddItem'))
+    @intent_handler(IntentBuilder("").require("Add").require('Item').require("List"))
     def add_item_to_list(self, message):
-        self.speak_dialog("shopping.list.add")
+        item = message.data.get('Item')
+        listName = message.data.get('List')
+        self.speak_dialog("list.add", data={"item": item, "List": listName})
+        # Now to add backend code for add
+
+    def remove_item_from_list(self, message):
+        pass
+
+    def clear_list(self,message):
+        pass
+
+    def send_list(self, message):
+        pass
 
 
 def create_skill():
