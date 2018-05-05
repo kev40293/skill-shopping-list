8ea1cb5fd316d346bac211d1b8674a8d4ae7c103
Kevin Brandstatter <kevin@kbrandstatter.com>
2018-05-05 11:24:25 -0500

    Added basic intent with no functionality

--- __init__.py
+++ __init__.py
@@ -4,11 +4,15 @@ from mycroft import MycroftSkill, intent_handler
 
 class ShoppingListSkill(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super(ShoppingListSkill, self).__init__(name="ShoppingListSkill")
 
-    @intent_handler(IntentBuilder().require('ShoppingList'))
+    @intent_handler(IntentBuilder("").require('ShoppingList'))
     def handle_shopping_list(self, message):
         self.speak_dialog('shopping.list')
+
+    @intent_handler(IntentBuilder("").require('AddItem'))
+    def add_item_to_list(self, message):
+        self.speak_dialog("shopping.list.add")
 
 
 def create_skill():
